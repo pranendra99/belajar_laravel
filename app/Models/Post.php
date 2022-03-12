64e52f331e9beee0fbc700f2968ad2026c239348
pranendra99 <evanfauzi0@gmail.com>
2022-03-12 17:36:21 +0700

    connect to DB

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,61 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    private static $posts = [
-        [
-            "title" => "Blog Post 1",
-            "slug" => "blog-post-1",
-            "author" => "John Doe",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Minus quis quaerat officiis accusamus impedit atque excepturi amet! Distinctio, 
-            nam explicabo! Vel velit molestiae necessitatibus sapiente! Nisi esse voluptatibus sint omnis!",
-        ],
-        [
-            "title" => "Blog Post 2",
-            "slug" => "blog-post-2",
-            "author" => "Evan Smith",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Facilis beatae consequatur incidunt sequi nam rerum quod eligendi a 
-            vel neque pariatur aliquid, error, nostrum, magnam distinctio necessitatibus 
-            totam ducimus. Laboriosam architecto iure eveniet voluptatum in quisquam inventore, 
-            eos cupiditate esse, alias odio asperiores obcaecati nisi excepturi quae ad fuga 
-            accusamus cumque necessitatibus. Aperiam nulla sit, voluptate illum minima ipsam 
-            hic illo facere cum in obcaecati quas architecto vero dicta libero dolorem repellendus? 
-            Cupiditate, laudantium sequi omnis, ad obcaecati optio, ipsum vitae architecto nam quos 
-            unde quasi harum ducimus voluptatem enim et laborum tenetur? Accusamus nulla quaerat itaque, 
-            dolores molestiae quidem.",
-        ],
-        [
-            "title" => "Blog Post 3",
-            "slug" => "blog-post-3",
-            "author" => "Jane Smith",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis 
-            corporis maiores sint sapiente voluptatem rerum dolor omnis reprehenderit placeat, 
-            temporibus quisquam possimus laboriosam. Ullam non quaerat iure, ipsum laborum quo 
-            natus asperiores. Adipisci pariatur totam possimus quasi tempora nobis dolores? Vero 
-            tempore et soluta. Eos nobis reprehenderit quasi numquam a.",
-        ],
+    use HasFactory;
+
+    // protected $fillable = [
+    //     'title', 
+    //     'excerpt',
+    //     'body',
+    // ];
+
+    protected $guarded = [
+        'id',
     ];
-
-    public static function all()
-    {
-        return collect(self::$posts);
-    }
-
-    public static function find($slug)
-    {
-        $blog_posts = static::all();
-        // $post = [];
-        // foreach ($blog_posts as $blog_post) {
-        //     if ($blog_post['slug'] == $slug) {
-        //         $post = $blog_post;
-        //     }
-        // }
-        
-        return $blog_posts->firstWhere('slug', $slug);
-    }
-
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 
     <article>
-        <h2>{{$posts["title"]}}</h2>
-        <h5>{{$posts["author"]}}</h5>
-        <p>{{$posts["body"]}}</p>
+        <h2>{{ $posts->title }}</h2>
+        {{-- <h5>{{$posts["author"]}}</h5> --}}
+        <p>{!! $posts->body !!}</p>
     </article>
 
     <a href="/posts">Kembali</a>
